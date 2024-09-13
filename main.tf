fd169666a8ce5de590fbe769353e39b8e504b0d5
Ericlys Moreira <ericlysm@gmail.com>
2024-09-13 00:24:11 -0300

    chore: add workspaces

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "ericlys-bucket-iac"
+  bucket = "ericlys-bucket-iac-${terraform.workspace}"
 
   tags= {
     Name = "Primeiro bucket"
     Iac = true
+    Context = "${terraform.workspace}"
   }
 }

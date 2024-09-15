950231ca419ab1c913f3dcca9862af6de9af771c
Ericlys Moreira <ericlysm@gmail.com>
2024-09-15 00:19:21 -0300

    chore: add terraform config s3 state

--- providers.tf
+++ providers.tf
@@ -5,9 +5,24 @@ terraform {
       version = "5.66.0"
     }
   }
+  backend "s3" {
+    bucket  = "ericlys-state-bucket-tf"
+    region  = "us-east-2"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+
 }
 
 provider "aws" {
   profile = "AdministratorAccess-217127483231"
   region  = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.state_bucket
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "state_bucket" {
+  type        = string
+  default     = "ericlys-state-bucket-tf"
+  description = "Bucket com o estado"
+}

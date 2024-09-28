61efd6a9ccdb4a4144bf5ca0b0a6863d6bd1dbee
Ericlys Moreira <ericlysm@gmail.com>
2024-09-28 10:49:24 -0300

    chore: add commit

--- modules/cloudfront/datasources.tf
+++ modules/cloudfront/datasources.tf
@@ -1,3 +1,3 @@
-data "aws_cloudfront_distribution" "cloudfront" {
+data "aws_cloudfront_distribution" "cloudfront" { //precisa que o recurso já esteja criado
   id = aws_cloudfront_distribution.cloudfront.id
 }
